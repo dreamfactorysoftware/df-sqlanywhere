7302f74ab58e3690b872a87d7b985093a40a0164
Lee Hicks <leehicks@dreamfactory.com>
2016-05-11 14:43:19 -0400

    adding config help

--- src/Models/SqlAnywhereDbConfig.php
+++ src/Models/SqlAnywhereDbConfig.php
@@ -11,21 +11,16 @@ class SqlAnywhereDbConfig extends SqlDbConfig
 {
     public static function getDriverName()
     {
-        if (substr(PHP_OS, 0, 3) == 'WIN') {
-            return 'sqlsrv';
-        } else {
-            return 'dblib';
-        }
+        return 'dblib';
     }
 
     public static function getDefaultDsn()
     {
-        if (substr(PHP_OS, 0, 3) == 'WIN') {
-            // http://php.net/manual/en/ref.pdo-sqlsrv.connection.php
-            return 'sqlsrv:Server=localhost,1433;Database=db';
-        } else {
-            // http://php.net/manual/en/ref.pdo-dblib.connection.php
-            return 'dblib:host=localhost:1433;dbname=database;charset=UTF-8';
-        }
+        return 'dblib:host=localhost:2638;dbname=database;charset=UTF-8';
+    }
+
+    public static function getDefaultPort()
+    {
+        return 2638;
     }
 }

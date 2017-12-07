ed0c0e29bdf3d5b9eb68cb73fa1c23aed0375946
Lee Hicks <leehicks@dreamfactory.com>
2017-12-07 15:33:03 -0500

    Separating resources from resource handlers.

--- src/Services/SqlAnywhere.php
+++ src/Services/SqlAnywhere.php
@@ -2,6 +2,8 @@
 
 namespace DreamFactory\Core\SqlAnywhere\Services;
 
+use DreamFactory\Core\SqlDb\Resources\StoredFunction;
+use DreamFactory\Core\SqlDb\Resources\StoredProcedure;
 use DreamFactory\Core\SqlDb\Services\SqlDb;
 
 /**
@@ -32,5 +34,23 @@ class SqlAnywhere extends SqlDb
             }
         }
         parent::adaptConfig($config);
+    }
+
+    public function getResourceHandlers()
+    {
+        $handlers = parent::getResourceHandlers();
+
+        $handlers[StoredProcedure::RESOURCE_NAME] = [
+            'name'       => StoredProcedure::RESOURCE_NAME,
+            'class_name' => StoredProcedure::class,
+            'label'      => 'Stored Procedure',
+        ];
+        $handlers[StoredFunction::RESOURCE_NAME] = [
+            'name'       => StoredFunction::RESOURCE_NAME,
+            'class_name' => StoredFunction::class,
+            'label'      => 'Stored Function',
+        ];
+
+        return $handlers;
     }
 }
